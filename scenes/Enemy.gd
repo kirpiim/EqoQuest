f7a012d8a121eae0d2ab7eb234b75cdbe699e8e5
Your Name <your@email.com>
2025-08-31 16:30:45 +0800

    added directions for enemy so that I can choose which direction the enemy would start with when the game runs

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-#enum Direction{RIGHT, LEFT}
-#export(Direction) var startDirection
+enum Direction{RIGHT, LEFT}
+export(Direction) var startDirection
 
 var maxSpeed = 25
 var velocity = Vector2.ZERO
@@ -9,9 +9,9 @@ var direction = Vector2.ZERO
 var gravity = 500
 
 #
-#func _ready():
-#	direction = Vector2.RIGHT if startDirection == Direction.RIGHT else Vector.LEFT
-#	$GoalDetector.connect("area_entered", self, "on_goal_entered")
+func _ready():
+	direction = Vector2.RIGHT if startDirection == Direction.RIGHT else Vector2.LEFT
+	$GoalDetector.connect("area_entered", self, "on_goal_entered")
 
 
 
@@ -22,5 +22,5 @@ func _process(delta):
 	
 	$AnimatedSprite.flip_h = true if direction.x > 0 else false
 	
-#func on_goal_entered(_area2D):
-#	direction *= -1;
+func on_goal_entered(_area2D):
+	direction *= -1;
