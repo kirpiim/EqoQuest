cb2c57c3a62e3ee5ae8a1aa179f3b7904a42ae17
kiirpim <190756791+kirpiim@users.noreply.github.com>
2025-09-04 19:43:48 +0800

    feat(baselevel): add coin tracking and signal emission
    - Declared `coin_total_changed` signal
    - Added `totalCoins` and `collectedCoins` variables
    - Emit signal with total and collected values on coin pickup
    - Implemented `coin_total_changed(newTotal)` for updating totals dynamically
    - Initialize coin total in `_ready()` by counting nodes in "coin" group
    - Implemented `coin_total_changed(newTotal)` to update total coins dynamically
    - Emit `coin_total_changed(total, collected)` signal whenever values update
    - Initialize `total_coins` in `_ready()` by counting nodes in "coin" group

--- scenes/BaseLevel.gd
+++ scenes/BaseLevel.gd
@@ -1,17 +1,26 @@
 extends Node2D
 
+signal coin_total_changed
 
 var playerScene = preload("res://scenes//Player.tscn")
 var spawnPosition = Vector2.ZERO
 var currentPlayerNode = null
-
+var totalCoins = 0
+var collectedCoins = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	spawnPosition = $Player.global_position
 	register_player($Player)
+	coin_total_changed(get_tree().get_nodes_in_group("coin").size())
 
-
+func coin_collected():
+	collectedCoins += 1
+	emit_signal("coin_total_changed", totalCoins, collectedCoins)
+	
+func coin_total_changed(newTotal):
+	totalCoins = newTotal
+	emit_signal("coin_total_changed", totalCoins, collectedCoins)
 
 func register_player(player):
 	currentPlayerNode = player
