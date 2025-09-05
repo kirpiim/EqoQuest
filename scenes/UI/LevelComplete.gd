99a65dbcf5e2f847525e038e41b66fa02d059418
Your Name <your@email.com>
2025-09-05 16:24:07 +0800

    Add LevelComplete UI scene with theme support
    - Created LevelComplete.tscn as CanvasLayer with label and Next Level button
    - Added LevelComplete.gd to handle button press and trigger LevelManager.increment_level()
    - Updated theme.tres with default font and MarginContainer constants

---
+++ scenes/UI/LevelComplete.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+
+
+func _ready():
+	$MarginContainer/VBoxContainer/Button.connect("pressed", self, "on_button_pressed")
+
+
+func on_button_pressed():
+	$"/root/LevelManager".increment_level()
