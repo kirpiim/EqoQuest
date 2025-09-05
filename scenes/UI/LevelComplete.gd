a421b56b73c5f85f08408047cab0154582c8520c
kiirpim <190756791+kirpiim@users.noreply.github.com>
2025-09-05 17:25:16 +0800

    Update LevelComplete.gd for new node hierarchy
    - Updated Button signal connection to use new path:
      $PanelContainer/MarginContainer/VBoxContainer/Button
    - Removed old connection to $MarginContainer/VBoxContainer/Button

--- scenes/UI/LevelComplete.gd
+++ scenes/UI/LevelComplete.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 
 
 func _ready():
-	$MarginContainer/VBoxContainer/Button.connect("pressed", self, "on_button_pressed")
+	$PanelContainer/MarginContainer/VBoxContainer/Button.connect("pressed", self, "on_button_pressed")
 
 
 func on_button_pressed():
